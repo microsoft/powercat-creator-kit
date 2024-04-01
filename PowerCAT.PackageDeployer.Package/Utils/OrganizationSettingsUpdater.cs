ec5864d082372db85866d722e2670e676a7f52cf
Rajeev Pentyala <rajeevpe@microsoft.com>
2024-04-01 10:41:36 +0530

    Checking-in PDPKG project for AppSource

---
+++ PowerCAT.PackageDeployer.Package/Utils/OrganizationSettingsUpdater.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+/// <summary>
+/// Utility class for updating organization settings.
+/// </summary>
+public class OrganizationSettingsUpdater
+{
+    private IOrganizationService CrmSvc;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OrganizationSettingsUpdater"/> class.
+    /// </summary>
+    /// <param name="organizationService">The CRM organization service.</param>
+    public OrganizationSettingsUpdater(IOrganizationService organizationService)
+    {
+        CrmSvc = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+    }
+
+    /// <summary>
+    /// Updates organization settings for a specific setting.
+    /// </summary>
+    /// <param name="settingName">The name of the organization setting to update.</param>
+    /// <param name="settingValue">The new value for the organization setting.</param>
+    public void UpdateOrganizationSettings(string settingName, object settingValue)
+    {
+        try
+        {
+            // Validate input
+            if (string.IsNullOrWhiteSpace(settingName))
+            {
+                Console.WriteLine("Setting name cannot be null or empty.");
+                return;
+            }
+
+            // Retrieve organization settings
+            var query = new QueryExpression("organization");
+            query.ColumnSet.AddColumns(settingName);
+
+            var result = CrmSvc.RetrieveMultiple(query);
+
+            if (result.Entities != null && result.Entities.Count > 0)
+            {
+                var environmentSettings = result.Entities[0];
+
+                // Check if settingValue is a boolean
+                if (settingValue is string stringValue && bool.TryParse(stringValue, out var boolValue))
+                {
+                    // Convert string value to boolean
+                    environmentSettings[settingName] = boolValue;
+                }
+                else
+                {
+                    // Update the specified setting
+                    environmentSettings[settingName] = settingValue;
+                }
+
+                // Update the organization settings
+                CrmSvc.Update(environmentSettings);
+
+                // Log or handle success
+                Console.WriteLine($"Organization settings updated successfully. {settingName}: {settingValue}");
+            }
+            else
+            {
+                // Handle the case where no organization settings were found
+                Console.WriteLine("No organization settings found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions
+            Console.WriteLine($"Error updating organization settings: {ex.Message}");
+        }
+    }
+}
---
+++ PowerCAT.PackageDeployer.Package/Utils/PacPackagesInstaller.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xrm.Tooling.PackageDeployment.CrmPackageExtentionBase;
+using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+/// <summary>
+/// Utility class for installing packages
+/// </summary>
+public class PacPackagesInstaller
+{
+    private string EnvironmentId;
+
+    private TraceLogger Logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PacPackagesInstaller"/> class.
+    /// </summary>
+    /// <param name="organizationService">The CRM organization service.</param>
+    public PacPackagesInstaller(string environmentId, TraceLogger packageLog)
+    {
+        EnvironmentId = environmentId;
+        Logger = packageLog;
+    }
+
+    /// <summary>
+    /// Installs a package using the Power Platform CLI (pac) for the specified environment and package name.
+    /// </summary>
+    /// <param name="packageName">The name of the package to install.</param>
+    public void InstallPackage(string packageName)
+    {
+        string pacCommand = $"pac application install --environment {EnvironmentId} --application-name {packageName}";
+        Console.WriteLine($"Triggering command {pacCommand}");
+        Logger.Log($"Triggering command {pacCommand}");
+        RunPowerShellCommand(pacCommand);
+    }
+
+    /// <summary>
+    /// Executes a PowerShell command and logs the output or errors.
+    /// </summary>
+    /// <param name="command">The PowerShell command to execute.</param>
+    public void RunPowerShellCommand(string command)
+    {
+        try
+        {
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+                // use "AddScript" to add the PowerShell command
+                PowerShellInstance.AddScript(command);
+
+                // invoke execution on the pipeline (collecting output)
+                Collection<PSObject> PSOutput = PowerShellInstance.Invoke();
+
+                // check for errors
+                if (PowerShellInstance.HadErrors)
+                {
+                    foreach (ErrorRecord error in PowerShellInstance.Streams.Error)
+                    {
+                        Console.WriteLine($"PowerShell Error: {error}");
+                        Logger.Log($"PowerShell Error while executing command : {command} Error : {error}");
+                    }
+                }
+                else
+                {
+                    // loop through each output object item
+                    foreach (PSObject outputItem in PSOutput)
+                    {
+                        // if null was dumped to the pipeline during the script, then a null object was added here
+                        if (outputItem != null)
+                        {
+                            // to get the .NET type of the output item
+                            Console.WriteLine(outputItem.BaseObject.GetType().FullName);
+
+                            // to get the string representation of the output item
+                            Console.WriteLine(outputItem.BaseObject.ToString() + "\n");
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle other exceptions
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Logger.Log($"PowerShell Error while executing command : {command} Error : {ex.Message}");
+        }
+    }
+}
